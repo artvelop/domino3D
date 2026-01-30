dd4f80c0b953ccc60f479a8f49d542e9b97a8d99
artvelop <richcode99@gmail.com>
2026-01-30 15:57:48 +0900

    feat: camera 위치 조정

--- src/modules/Camera.ts
+++ src/modules/Camera.ts
@@ -6,7 +6,7 @@ export class Camera {
   constructor() {
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
-    this.camera.position.y = 90
-    this.camera.position.z = 20
+    this.camera.position.y = 1.8
+    this.camera.position.z = 2.5
   }
 }
